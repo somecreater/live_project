36063382abff4e69306e6b2cbb70418253242c8f
somecreater <127456520+somecreater@users.noreply.github.com>
2025-11-11 23:22:25 +0900

    로그인 요청 DTO 생성, 회원 관련 등록, 로그인, 정보 가져오기 서비스 메소드 구현 및 컨트롤러 클래스 생성

---
+++ src/main/java/com/live/main/user/database/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.live.main.user.database.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+  public String id;
+  public String pass;
+}
--- src/main/java/com/live/main/user/service/UserService.java
+++ src/main/java/com/live/main/user/service/UserService.java
@@ -5,14 +5,18 @@ import com.live.main.user.database.dto.UserDto;
 import com.live.main.user.database.entity.UsersEntity;
 import com.live.main.user.database.mapper.UserMapper;
 import com.live.main.user.database.repository.UserRepository;
+import com.live.main.user.jwt.JwtService;
 import com.live.main.user.service.Interface.UserServiceInterface;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -20,10 +24,13 @@ import java.util.Optional;
 @Slf4j
 public class UserService implements UserServiceInterface, UserDetailsService {
 
+  private final JwtService jwtService;
+  private final PasswordEncoder passwordEncoder;
   private final UserRepository userRepository;
   private final UserMapper userMapper;
 
   //사용자 아이디로 사용자 인증 객체 생성
+  @Transactional(readOnly = true)
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     Optional<UsersEntity> optionalUsers= userRepository.findByLoginId(username);
@@ -33,6 +40,8 @@ public class UserService implements UserServiceInterface, UserDetailsService {
     return new CustomUserDetails(optionalUsers.get());
   }
 
+  //회원 등록 기능
+  @Transactional
   @Override
   public UserDto RegisterUser(UserDto userDto) {
     UsersEntity users=new UsersEntity();
@@ -54,31 +63,61 @@ public class UserService implements UserServiceInterface, UserDetailsService {
       return null;
     }
 
+    userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+    userDto.setCreatedAt(LocalDateTime.now());
+    userDto.setUpdatedAt(LocalDateTime.now());
     users=userMapper.toEntity(userDto);
     UsersEntity newUser = userRepository.save(users);
     return userMapper.toDto(newUser);
   }
 
+  //로그인 기능
+  @Transactional(readOnly = true)
   @Override
   public UserDto LoginUser(String id, String pass) {
-    return null;
+    UsersEntity loginUser=userRepository.findByLoginId(id).orElse(null);
+    if(loginUser == null){
+      throw new IllegalArgumentException("아이디나 비밀번호가 틀렸습니다.");
+    }
+    if(!passwordEncoder.matches(pass, loginUser.getPassword())){
+      throw new IllegalArgumentException("아이디나 비밀번호가 틀렸습니다.");
+    }
+
+    return userMapper.toDto(loginUser);
   }
 
+  // 현재는 아이디만 사용하지만 나중에는 채널명 등 여러 정보를 이용해서
+  // 회원정보를 가지고 올 수도 있다.
+  // 회원 정보 가져오기 기능
+  @Transactional(readOnly = true)
   @Override
   public UserDto GetUserInfo(UserDto userDto) {
-    return null;
+    UsersEntity usersEntity = null;
+    String LoginId = userDto.getLoginId();
+    if(LoginId != null){
+      usersEntity = userRepository.findByLoginId(LoginId).orElse(null);
+    }
+
+    if(usersEntity == null){
+      return null;
+    }
+    return userMapper.toDto(usersEntity);
   }
 
+  //회원 정보 수정 기능
   @Override
   public UserDto UpdateUser(UserDto userDto) {
+    
     return null;
   }
 
+  //비밀번호 수정 기능
   @Override
   public boolean UpdatePassword(UserDto userDto, String pass) {
     return false;
   }
 
+  //회원 탈퇴 기능
   @Override
   public boolean DeleteUser(UserDto userDto) {
     return false;
