46218068dac962941c22ae44e0b29eb6df763b49
somecreater <127456520+somecreater@users.noreply.github.com>
2025-12-05 13:03:01 +0900

    채널 서비스 메소드 구현중

--- src/main/java/com/live/main/channel/database/repository/ChannelRepository.java
+++ src/main/java/com/live/main/channel/database/repository/ChannelRepository.java
@@ -5,12 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChannelRepository extends JpaRepository<ChannelEntity, Long> {
-    Page<ChannelEntity> findByName(String name, Pageable pageable);
-
-    Page<ChannelEntity> findByDescription(String description, Pageable pageable);
-
     Page<ChannelEntity> findByNameLike(String name, Pageable pageable);
 
     Page<ChannelEntity> findByDescriptionLike(String description, Pageable pageable);
+
+    boolean existsByNameOrUsers_LoginId(String name, String loginId);
+
+    Optional<ChannelEntity> findByName(String name);
+
+    void deleteByName(String name);
 }
--- src/main/java/com/live/main/channel/service/ChannelService.java
+++ src/main/java/com/live/main/channel/service/ChannelService.java
@@ -5,6 +5,8 @@ import com.live.main.channel.database.entity.ChannelEntity;
 import com.live.main.channel.database.mapper.ChannelMapper;
 import com.live.main.channel.database.repository.ChannelRepository;
 import com.live.main.channel.service.Interface.ChannelServiceInterface;
+import com.live.main.common.database.dto.ErrorCode;
+import com.live.main.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -12,6 +14,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 
 @Service
@@ -35,35 +39,79 @@ public class ChannelService implements ChannelServiceInterface {
   @Override
   @Transactional(readOnly = true)
   public Page<ChannelDto> getChannelList(int page, int size, String type, String keyword) {
-      Pageable pageable = PageRequest.of(page, size);
-      Page<ChannelEntity> channelEntityPage = null;
-      if (type.equals("name")) {
-          channelEntityPage = channelRepository.findByNameLike(keyword,pageable);
-      } else if (type.equals("description")) {
-          channelEntityPage = channelRepository.findByDescriptionLike(keyword, pageable);
-      }
-      if (channelEntityPage != null) {
-          return channelEntityPage.map(channelMapper::toDto);
-      }else{
-          return null;
-      }
+    Pageable pageable = PageRequest.of(page, size);
+    Page<ChannelEntity> channelEntityPage = null;
+    if (type.equals("name")) {
+      channelEntityPage = channelRepository.findByNameLike(keyword,pageable);
+    } else if (type.equals("description")) {
+      channelEntityPage = channelRepository.findByDescriptionLike(keyword, pageable);
+    }
+    if (channelEntityPage != null) {
+      return channelEntityPage.map(channelMapper::toDto);
+    }else{
+      return null;
+    }
   }
 
   @Override
   @Transactional
   public ChannelDto createChannel(ChannelDto channelDto) {
-    return null;
+    if(channelDto.getName().isEmpty()
+    || channelDto.getUser_login_id().isEmpty()){
+      throw new CustomException(ErrorCode.BAD_REQUEST);
+    }
+    boolean isExist= channelRepository.existsByNameOrUsers_LoginId(
+      channelDto.getName(), channelDto.getUser_login_id()
+    );
+    if(isExist){
+      throw new CustomException(ErrorCode.BAD_REQUEST);
+    }
+    ChannelEntity entity=channelMapper.toEntity(channelDto);
+    entity.setCreatedAt(LocalDateTime.now());
+    entity.setUpdatedAt(LocalDateTime.now());
+    ChannelEntity newChannel = channelRepository.save(entity);
+    return channelMapper.toDto(newChannel);
   }
 
   @Override
   @Transactional
   public ChannelDto updateChannel(ChannelDto channelDto) {
-    return null;
+    if(channelDto.getName().isEmpty()
+    || channelDto.getUser_login_id().isEmpty()){
+      throw new CustomException(ErrorCode.BAD_REQUEST);
+    }
+    ChannelEntity entity= channelRepository.findByName(channelDto.getName()).orElse(null);
+    if(entity == null){
+      throw new CustomException(ErrorCode.BAD_REQUEST);
+    }else if(entity.getUsers().getLoginId().compareTo(channelDto.getUser_login_id())!=0){
+      throw new CustomException(ErrorCode.BAD_REQUEST);
+    }
+    entity.setName(channelDto.getName());
+    entity.setDescription(channelDto.getDescription());
+    entity.setUpdatedAt(LocalDateTime.now());
+    ChannelEntity updateEntity= channelRepository.save(entity);
+    return channelMapper.toDto(updateEntity);
   }
 
   @Override
   @Transactional
   public boolean deleteChannel(ChannelDto channelDto) {
-    return false;
+    try {
+        if (channelDto.getName().isEmpty()
+                || channelDto.getUser_login_id().isEmpty()) {
+            throw new CustomException(ErrorCode.BAD_REQUEST);
+        }
+        ChannelEntity entity = channelRepository.findByName(channelDto.getName()).orElse(null);
+        if (entity == null) {
+            throw new CustomException(ErrorCode.BAD_REQUEST);
+        } else if (entity.getUsers().getLoginId().compareTo(channelDto.getUser_login_id()) != 0) {
+            throw new CustomException(ErrorCode.BAD_REQUEST);
+        }
+        channelRepository.deleteByName(channelDto.getName());
+    } catch (Exception e) {
+        e.printStackTrace();
+        throw new CustomException(ErrorCode.BAD_REQUEST);
+    }
+    return true;
   }
 }
