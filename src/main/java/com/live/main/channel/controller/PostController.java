15bfb82aa3e5782b1c050614eb34957d1f6d5f7a
somecreater <127456520+somecreater@users.noreply.github.com>
2025-12-18 08:54:21 +0900

    채널 게시글 읽기, 페이지 읽기, 쓰기 Controller 메소드 작성

--- src/main/java/com/live/main/channel/controller/PostController.java
+++ src/main/java/com/live/main/channel/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.live.main.channel.controller;
 
+import com.live.main.channel.database.dto.ChannelDto;
 import com.live.main.channel.database.dto.PostDeleteRequest;
 import com.live.main.channel.database.dto.PostDto;
 import com.live.main.channel.database.dto.PostSearchRequest;
@@ -8,12 +9,14 @@ import com.live.main.channel.service.Interface.PostServiceInterface;
 import com.live.main.user.database.dto.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -29,7 +32,22 @@ public class PostController {
     @PathVariable Long post_id
   ){
     Map<String, Object> result= new HashMap<>();
-
+    ChannelDto channelDto= channelService.getChannelInfoUser(principal.getUserid());
+    PostDto postDto=postService.readPostByOwner(post_id);
+    if(postDto == null){
+      result.put("result", false);
+    }
+    else if(channelDto !=null && Objects.equals(postDto.getChannel_name(), channelDto.getName())){
+      result.put("result", true);
+      result.put("post", postDto);
+    }else{
+      if(!postDto.isVisibility()){
+        result.put("result", false);
+      }else{
+        result.put("result", true);
+        result.put("post", postDto);
+      }
+    }
     return ResponseEntity.ok(result);
   }
 
@@ -39,7 +57,32 @@ public class PostController {
     @RequestBody PostSearchRequest postSearchRequest
   ){
     Map<String, Object> result= new HashMap<>();
+    Page<PostDto> postDtoPage=null;
+    ChannelDto channelDto= channelService.getChannelInfoUser(principal.getUserid());
+    if(channelDto!=null && Objects.equals(channelDto.getName(), postSearchRequest.getChannel_name())){
+      postDtoPage=postService.readPostPageByOwner(
+        postSearchRequest.getPage(),
+        postSearchRequest.getSize(),
+        postSearchRequest.getType(),
+        postSearchRequest.getKeyword(),
+        postSearchRequest.getChannel_name()
+      );
+    }else{
+      postDtoPage=postService.readPostPage(
+        postSearchRequest.getPage(),
+        postSearchRequest.getSize(),
+        postSearchRequest.getType(),
+        postSearchRequest.getKeyword(),
+        postSearchRequest.getChannel_name()
+      );
+    }
 
+    if(postDtoPage == null){
+      result.put("result", false);
+    }else{
+      result.put("result", true);
+      result.put("post_page", postDtoPage);
+    }
     return ResponseEntity.ok(result);
   }
 
@@ -50,7 +93,14 @@ public class PostController {
   )
   {
     Map<String, Object> result= new HashMap<>();
-
+    ChannelDto channelDto= channelService.getChannelInfoUser(principal.getUserid());
+    if(channelDto!=null && Objects.equals(postDto.getChannel_name(), channelDto.getName())){
+      PostDto newPost= postService.writePost(postDto);
+      result.put("result", true);
+      result.put("new_post", newPost);
+    }else{
+      result.put("result", false);
+    }
     return ResponseEntity.ok(result);
   }
 
