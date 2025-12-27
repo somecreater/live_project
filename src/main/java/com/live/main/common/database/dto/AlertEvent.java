aef8db85827871135d1390371894402f9143bc6c
somecreater <dbstjrgy123@gmail.com>
2025-12-27 22:58:16 +0900

    실시간 알림 객체 개선

--- src/main/java/com/live/main/common/database/dto/AlertEvent.java
+++ src/main/java/com/live/main/common/database/dto/AlertEvent.java
@@ -5,14 +5,12 @@ import lombok.Getter;
 @Getter
 public class AlertEvent {
   private final AlertType type;
-  private final String sender;
-  private final String receiver;
+  private final String publisher;
   private final String content;
 
-  public AlertEvent(String type, String sender, String receiver, String content){
+  public AlertEvent(String type, String publisher, String content){
     this.type=AlertType.valueOf(type);
-    this.sender=sender;
-    this.receiver=receiver;
+    this.publisher=publisher;
     this.content=content;
   }
 }
