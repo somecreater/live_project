1106c8a8917bf34edae735562fd4557a9b23c01c
somecreater <dbstjrgy123@gmail.com>
2026-03-08 23:43:21 +0900

    동영상 controller 일부 메소드 추가

--- src/main/java/com/live/main/video/controller/VideoController.java
+++ src/main/java/com/live/main/video/controller/VideoController.java
@@ -1,11 +1,20 @@
 package com.live.main.video.controller;
 
 
+import com.live.main.user.database.dto.CustomUserDetails;
+import com.live.main.video.database.dto.VideoDto;
 import com.live.main.video.service.Interface.VideoServiceInterface;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/video")
@@ -15,5 +24,13 @@ public class VideoController {
 
   private final VideoServiceInterface videoService;
 
+  @PostMapping("/upload-url")
+  public ResponseEntity<?> getVideoUploadUrl(
+          @AuthenticationPrincipal CustomUserDetails principal,
+          @RequestBody VideoDto videoDto) {
+    Map<String ,Object> result = new HashMap<>();
+
+    return ResponseEntity.ok(result);
+  }
 
 }
